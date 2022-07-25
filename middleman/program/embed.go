9ea705f75ee8704f012195119d5bc693918032ab
Onur YURDUPAK <onuryurdupak@gmail.com>
2022-07-25 13:17:00 +0300

    Update help text

--- middleman/program/embed.go
+++ middleman/program/embed.go
@@ -18,34 +18,14 @@ const (
 	helpPrompt = `Run 'middleman -h' for help.`
 
 	helpMessage = `
-If your terminal does not render styles properly, run 'interpolator -hr' to view in style-free mode.
+If your terminal does not render styles properly, run 'interpolator -hr' to view help in raw mode.
 
 <b><u><yellow>PARAMETERS:</yellow></u></b>
 <b><yellow>-v</yellow></b>: Show version info.
 <b><yellow>-p</yellow></b>: Run on custom port.
-<b><yellow>--raw</b></yellow>: Print request & response bodies without prettifying.
+<b><yellow>--raw</b></yellow>: Print request & response bodies without styles.
 
-<b><u><yellow>DESCRIPTION</yellow></u></b>
-middleman <b><yellow>(B)</yellow></b> acts as an intermediary which forwards requests from client <b><yellow>(A)</yellow></b> to server <b><yellow>(C)</yellow></b> and returns the server's response to client <b><yellow>(A)</yellow></b>.
-When <b>middleman</b> is run on your deployment environment <b><yellow>(B)</yellow></b> (and allowed to host on port 8080) you can make calls like:
-
-<green>POST: http: //{address-of-B}:8080/https://{address-of-C}</green>
-
-instead of:
-
-<green>POST: https: //{address-of-C}</green>
-
-from your local client application. It will redirect request headers and request body.
-
-<b><u><yellow>USAGE</yellow></u></b>
-<b>client <b><yellow>(A)</yellow></b> -> middleman <b><yellow>(B)</yellow></b> -> server <b><yellow>(C)</yellow></b></b>
-
-You are developing & debugging a project on your local machine <b><yellow>(A)</yellow></b> which needs to make API calls to an application on a remote machine <b><yellow>(C)</yellow></b>.You local machine <b><yellow>(A)</yellow></b> has no direct access to remote machine <b><yellow>(C)</yellow></b>, but your deployment environment <b><yellow>(B)</yellow></b> does.
-There may be similar circumstances where it is more practical to go this way instead of configuring & using a real http proxy.
-
-To run middleman on a custom port use -p argument. Example:
-
-<green>middleman -p 5000</green>
+Readme is availabile at: https://github.com/onuryurdupak/middleman#readme
 `
 )
 
